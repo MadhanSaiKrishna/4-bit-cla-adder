f210724b2a896d6dd1f091098b33eeb2fcb75697
MadhanSaiKrishna <madhansaikrishna@outlook.com>
2024-11-26 03:05:40 +0530

    Fixed the issue with verilog code (wrong port declaration and assignment)

--- cla_adder_struct.v
+++ cla_adder_struct.v
@@ -48,9 +48,11 @@ module adder(A_in, B_in, Cin, sum, Cout,clk);
     output [3:0]sum;
     output Cout;
 
-    wire [3:0]A,B;
-    wire P0,P1, P2, P3;
-    wire G0, G1,G2,G3;
+    wire [3:0] A, B;
+    wire P0, P1, P2, P3;
+    wire G0, G1, G2, G3;
+    wire C1, C2, C3;
+    wire pc0, p1p0c0, p1g0, p2p1p0c0, p2p1g0, p2g1, p3p2p1p0c0, p3p2p1g0, p3p2g1, p3g2;
 
     dff ff1(clk,A_in[0],A[0]);
     dff ff2(clk,A_in[1],A[1]);
@@ -63,18 +65,18 @@ module adder(A_in, B_in, Cin, sum, Cout,clk);
     dff ff8(clk,B_in[3],B[3]);
 
     
-    my_xor g1(P0, A[0], B[0]);
-    my_xor g2(P1, A[1], B[1]);
-    my_xor g3(P2, A[2], B[2]);
-    my_xor g4(P3, A[3], B[3]);
+    my_xor g1(A[0], B[0],P0);
+    my_xor g2(A[1], B[1],P1);
+    my_xor g3(A[2], B[2],P2);
+    my_xor g4(A[3], B[3],P3);
 
-    my_and g5(G0, A[0], B[0]);
-    my_and g6(G1, A[1], B[1]);
-    my_and g7(G2, A[2], B[2]);
-    my_and g8(G3, A[3], B[3]);
+    my_and g5(A[0], B[0], G0);
+    my_and g6(A[1], B[1], G1);
+    my_and g7(A[2], B[2], G2);
+    my_and g8(A[3], B[3], G3);
 
-    my_and g9(pc0, P0,Cin);
-    my_or g10(C1, pc0, G0);
+    my_and g9(P0,Cin,pc0);
+    my_or g10(pc0, G0,C1);
 
     and g11(p1p0c0, P1, P0, Cin);
     and g12(p1g0, P1, G0);
@@ -105,4 +107,44 @@ module adder(A_in, B_in, Cin, sum, Cout,clk);
     dff ff12(clk,sum_out[3],sum[3]);
     dff ff13(clk, Cout_out,Cout);
 
+endmodule
+
+module tb();
+
+reg [3:0] A,B;
+reg Cin;
+reg clk;
+
+wire [3:0] sum;
+wire Cout;
+integer i;
+
+adder dut(.A_in(A),.B_in(B),.Cout(Cout),.sum(sum),.Cin(Cin),.clk(clk));
+
+initial begin
+    A = 4'b0;
+    B = 4'b0;
+    Cin = 1'b0;
+end
+
+initial begin 
+    clk = 0;
+    forever #5 clk = ~clk;
+end
+
+initial begin 
+    for(i = 0; i< 16 ; i = i+1)
+    begin
+        A = i;
+        B = i;
+        Cin = 0;
+        #10;
+    end
+end
+
+initial begin
+    $monitor($time, " A = %4b , B = %4b , Cin = %b , Sum = %4b , Cout = %b, Clk : ", A, B, Cin, sum, Cout,clk);
+    #160;
+    $finish;
+end
 endmodule
